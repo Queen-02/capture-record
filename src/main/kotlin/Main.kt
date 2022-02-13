356891a2fdb47d14e7e47d62d6b02c74b6f2ff65
Queen-02 <queenzyblack176@gmail.com>
2022-02-13 12:18:34 +0300

    add changes made in main file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,50 @@
 fun main(args: Array<String>) {
     var studentName = arrayOf("James Kamau", "Princess Cheruto", "Zipporah Kananu", "Oluoch Obado", "Alan Kemboi", "Christine Oure")
-    var studentAge = 28
-    var studentCountry = "Kenya"
-    var studentWeight = 90
-    println(studentWeight)
-    var phoneNumber = "08009867"
-    println(phoneNumber)
+    var studentAge = arrayOf(28, 34, 22, 19, 25, 20)
+    var studentWeight = arrayOf("56kg", "69kg", "50kg", "86kg", "77kg", "60kg")
+    var phoneNumber = arrayOf("0720984567", "0800387965", "0719876345","0756578321", "011089345", "0800678934")
+    var kenyan = arrayOf(true, false, true, true, true, false)
 
-    if (studentAge == 29) {
-        println("Alan Kemboi")
-    }else if (studentWeight == 90) {
-        println("Oluoch Obado")
-    } else{
-        println("Christine Oure")
-    }
-    if (studentAge == 90) {
-        println(studentCountry)
-    }else{
-        println("is not Kenyan")
-    }
+    //printing starting the last student
+    println(studentName[5],)
+    println(studentAge[5])
+    println(studentWeight[5])
+    println(phoneNumber[5])
+    println(kenyan[5])
+
+    //second last
+    println(studentName[4],)
+    println(studentAge[4])
+    println(studentWeight[4])
+    println(phoneNumber[4])
+    println(kenyan[4])
+
+    //student3
+    println(studentName[3])
+    println(studentAge[3])
+    println(studentWeight[3])
+    println(phoneNumber[3])
+    println(kenyan[3])
+
+    //student2
+    println(studentName[2],)
+    println(studentAge[2])
+    println(studentWeight[2])
+    println(phoneNumber[2])
+    println(kenyan[2])
+
+    //student1
+    println(studentName[1],)
+    println(studentAge[1])
+    println(studentWeight[1])
+    println(phoneNumber[1])
+    println(kenyan[1])
+
+    //student0
+    println(studentName[0],)
+    println(studentAge[0])
+    println(studentWeight[0])
+    println(phoneNumber[0])
+    println(kenyan[0])
+
 }
