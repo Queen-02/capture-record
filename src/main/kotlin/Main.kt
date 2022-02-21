6e44987ba8670e0f5fb8d054515437520ae0d0c8
Queen-02 <queenzyblack176@gmail.com>
2022-02-21 19:19:14 +0300

    add changes made to file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,50 +1,19 @@
 fun main(args: Array<String>) {
-    var studentName = arrayOf("James Kamau", "Princess Cheruto", "Zipporah Kananu", "Oluoch Obado", "Alan Kemboi", "Christine Oure")
-    var studentAge = arrayOf(28, 34, 22, 19, 25, 20)
-    var studentWeight = arrayOf("56kg", "69kg", "50kg", "86kg", "77kg", "60kg")
-    var phoneNumber = arrayOf("0720984567", "0800387965", "0719876345","0756578321", "011089345", "0800678934")
-    var kenyan = arrayOf(true, false, true, true, true, false)
+    var studentName = "Full_name"
+    println(studentName)
+    var studentAge = 34
+    println(studentAge)
+    var studentWeight = "60kg"
+    println(studentWeight)
+    var phoneNumber = "0720987345"
+    println(phoneNumber)
+    var kenyan = true
+    println(kenyan)
+    var notKenyan = false
+    println(notKenyan)
 
-    //printing starting the last student
-    println(studentName[5],)
-    println(studentAge[5])
-    println(studentWeight[5])
-    println(phoneNumber[5])
-    println(kenyan[5])
 
-    //second last
-    println(studentName[4],)
-    println(studentAge[4])
-    println(studentWeight[4])
-    println(phoneNumber[4])
-    println(kenyan[4])
 
-    //student3
-    println(studentName[3])
-    println(studentAge[3])
-    println(studentWeight[3])
-    println(phoneNumber[3])
-    println(kenyan[3])
 
-    //student2
-    println(studentName[2],)
-    println(studentAge[2])
-    println(studentWeight[2])
-    println(phoneNumber[2])
-    println(kenyan[2])
-
-    //student1
-    println(studentName[1],)
-    println(studentAge[1])
-    println(studentWeight[1])
-    println(phoneNumber[1])
-    println(kenyan[1])
-
-    //student0
-    println(studentName[0],)
-    println(studentAge[0])
-    println(studentWeight[0])
-    println(phoneNumber[0])
-    println(kenyan[0])
 
 }
